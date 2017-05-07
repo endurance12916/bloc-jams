770ee256b2ec3db436e4a7736e8c554159049fa7
Zixing(Luke) Li <zl82@cornell.edu>
2017-05-07 17:45:41 -0400

    checkpoint-19-completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,89 @@
+// Example Album
+let albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [{
+            title: 'Blue',
+            duration: '4:26'
+        },
+        {
+            title: 'Green',
+            duration: '3:14'
+        },
+        {
+            title: 'Red',
+            duration: '5:01'
+        },
+        {
+            title: 'Pink',
+            duration: '3:21'
+        },
+        {
+            title: 'Magenta',
+            duration: '2:15'
+        }
+    ]
+};
+
+// Another Example Album
+let albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [{
+            title: 'Hello, Operator?',
+            duration: '1:01'
+        },
+        {
+            title: 'Ring, ring, ring',
+            duration: '5:01'
+        },
+        {
+            title: 'Fits in your pocket',
+            duration: '3:21'
+        },
+        {
+            title: 'Can you hear me now?',
+            duration: '3:14'
+        },
+        {
+            title: 'Wrong phone number',
+            duration: '2:15'
+        }
+    ]
+};
+
+// Third example Album
+let albumThird = {
+    title: `Can't come up with a title`,
+    artist: `Can't come up with an artist`,
+    label: 'idk',
+    year: '2017',
+    albumArtUrl: 'assets/images/album_covers/03.png',
+    songs: [{
+            title: 'First Song',
+            duration: '4:26'
+        },
+        {
+            title: 'Second Song',
+            duration: '3:14'
+        },
+        {
+            title: 'Third Song',
+            duration: '5:01'
+        },
+        {
+            title: 'Fourth Song',
+            duration: '3:21'
+        },
+        {
+            title: 'Fifth Song',
+            duration: '2:15'
+        }
+    ]
+};
